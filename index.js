ade5c4877d2e46f52416d0b25c8b39033a25a54f
ericvvv <henri.barun@gmail.com>
2022-04-05 15:36:56 -0700

    Complete letter reveal animation. To Do: fix multiple letter eval bug, add full word list, add error when guess not in word list, add further animations for win, adjust timing of win/lose modal.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-// Create global var to store word as it is being entered
-let CURRWORD = "";
+// TODO: Refactor document queries for "allKeys" into separate function?
+
+
+// Global var to store current solution
+let CURR_WORD = "";
+// Global var to store user guess as it is being entered
+let CURR_GUESS = "";
+// Global var to track index in master word list
+let WORD_INDEX = 0;
+// Global var to hold game state (true/false)
+let IS_PLAYING = true;
+// Global var to hold message modal
+let MODAL;
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Query for message modal
+    MODAL = document.querySelector(".modal-content");
+
+    // Initialize new game
+    startGame();
 
     // Add event listener to entire page to catch keyboard input
     document.addEventListener('keydown', event => parseInput(event.key));
@@ -17,34 +34,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function parseInput(key) {
 
-    // Set error modal display mode to 'none' whenever key is pressed
-    const modal = document.querySelector(".modal-content");
-    modal.style.display = 'none';
+    // Check that game is active
+    if (!IS_PLAYING) {
+        startGame();
+    } else {
+        // Set message modal display mode to 'none' whenever key is pressed
+        MODAL.style.display = 'none';
 
-    // Make sure active row animation is set to ""
-    const activeRow = getActiveRow();
-    activeRow.dataset.animation = "";
+        // Make sure active row animation is set to ""
+        const activeRow = getActiveRow();
+        activeRow.dataset.animation = "";
 
-    const currKey = key.toUpperCase();
-    const keyCode = currKey.charCodeAt(0);
+        // Convert key input to upper case, and then to ASCII code
+        const currKey = key.toUpperCase();
+        const keyCode = currKey.charCodeAt(0);
 
-    if (currKey == 'BACKSPACE' || currKey == 'DELETE') {
-        deleteLetter();
-    }
-    else if (currKey == 'ENTER') {
-        evaluateWord();
-    }
-    else if ((keyCode >= 65 && keyCode <= 90) && currKey.length == 1) {
+        // Parse whether input is delete, enter, or a letter guess
+        if (currKey == 'BACKSPACE' || currKey == 'DELETE') {
+            deleteLetter();
+        }
+        else if (currKey == 'ENTER') {
+            evaluateWord();
+        }
+        else if ((keyCode >= 65 && keyCode <= 90) && currKey.length == 1) {
 
-        CURRWORD += currKey;
+            CURR_GUESS += currKey;
 
-        if (CURRWORD.length > 5) {
-            CURRWORD = CURRWORD.slice(0, -1);
-        } else {
-            addLetter(currKey);
+            // Make sure we can't enter more than 5 letters per word
+            if (CURR_GUESS.length > 5) {
+                CURR_GUESS = CURR_GUESS.slice(0, -1);
+            } else {
+                addLetter(currKey);
+            }
         }
     }
-    
 }
 
 function addLetter(letter) {
@@ -57,8 +80,8 @@ function addLetter(letter) {
     // Change state of current tile to active='false'
     activeTile.dataset.active = 'false';
 
-    // Change text of currently active tile to currKey in uppercase
-    activeTile.textContent = letter;
+    // Change text of currently active tile to currKey
+    activeTile.firstElementChild.textContent = letter;
 
     // Add letter to tile's data-letter
     activeTile.dataset.letter = letter;
@@ -67,8 +90,7 @@ function addLetter(letter) {
     activeTile.dataset.animation = "pop";
 
     // If word length is less than five, get next tile after current one and change state to active='true'
-    if (CURRWORD.length < 5) {
-        console.log("CURRWORD length: ", CURRWORD.length);
+    if (CURR_GUESS.length < 5) {
         activeTile.nextElementSibling.dataset.active = 'true';
     }
 }
@@ -89,16 +111,16 @@ function deleteLetter() {
             const activeTile = activeRow.querySelector(".game-tile[data-active='true']");
             activeTile.dataset.active = 'false';
         }
-        // ...set the textContent of last tile in array to empty and set its active state to 'true'
+        // ...set the textContent of last active tile in array to empty and set its active state to 'true'
         const lastEntered = result[result.length - 1];
-        lastEntered.textContent = "";
+        lastEntered.firstElementChild.textContent = "";
         lastEntered.dataset.active = 'true';
         // Remove letter from tile's data-letter
         lastEntered.dataset.letter = "";
         // Set data-animation back to none
         lastEntered.dataset.animation = "";
         // Don't forget to remove the deleted letter from CURRWORD!
-        CURRWORD = CURRWORD.slice(0, -1);
+        CURR_GUESS = CURR_GUESS.slice(0, -1);
     }
 }
 
@@ -106,17 +128,74 @@ function evaluateWord() {
 
     // Query for currently active row
     const activeRow = getActiveRow();
+    // Query for all tiles in currently active row
+    const tiles = activeRow.querySelectorAll('.game-tile');
+    // Create array from tiles list
+    const tilesArray = Array.from(tiles);
     
-    if (CURRWORD.length < 5) {
+    // Make sure submitted guess is 5 chars long
+    if (CURR_GUESS.length < 5) {
         activeRow.dataset.animation = "shake";
-        const message = "Not Enough Letters";
-        displayError(message);
+        displayMessage("Not Enough Letters");
     } else {
+        // If current guess is correct...
+        if (CURR_GUESS == CURR_WORD) {
+            for (let i = 0; i < 5; i++) {
+                const currLetter = tilesArray[i].dataset.letter;
 
-        // TODO: word eval logic goes here //////////////////////////////////////
+                // Set temp color/status 
+                tilesArray[i].dataset.tempStatus = "correct";
+                    
+                // Set color/status for key in virtual keyboard
+                document.querySelector(`#keyboard div[data-letter=${currLetter}]`).dataset.status = "correct";
 
-        // Clear CURRWORD
-        CURRWORD = "";
+                animateReveal(tilesArray, i);
+            }
+
+            displayMessage("You Win!");
+            IS_PLAYING = false;
+
+        } else {
+            // Go through each letter of guess and compare to solution (avoids double-counting letters):
+
+            // Copy CURRWORD to temp var for comparison below
+            let tempWord = CURR_WORD;
+
+            for (let i = 0; i < 5; i++) {
+                // Store current letter of CURRGUESS string in var currLetter
+                const currLetter = tilesArray[i].dataset.letter;
+                // If the guessed letter is in the right position...
+                if (currLetter == tempWord[i]) {
+                    // Remove "correct" letter from tempWord
+                    tempWord = tempWord.replace(currLetter, " ");
+
+                    // Set temp color/status 
+                    tilesArray[i].dataset.tempStatus = "correct";
+                    
+                    // Set color/status for key in virtual keyboard
+                    document.querySelector(`#keyboard div[data-letter=${currLetter}]`).dataset.status = "correct";
+                }
+                // If the guessed letter is in the word, but in a different position...
+                else if (tempWord.includes(currLetter)) {
+                    // Remove "present" letter from tempWord
+                    tempWord = tempWord.replace(currLetter, " ");
+
+                    tilesArray[i].dataset.tempStatus = "present";
+                    document.querySelector(`#keyboard div[data-letter=${currLetter}]`).dataset.status = "present";
+                }
+                // If the guessed letter is not in the word...
+                else {
+                    tilesArray[i].dataset.tempStatus = "wrong";
+                    document.querySelector(`#keyboard div[data-letter=${currLetter}]`).dataset.status = "wrong";
+                }
+                
+                animateReveal(tilesArray, i);
+            }
+        }
+
+        // Reset to next row:
+        // Clear CURRGUESS
+        CURR_GUESS = "";
 
         // Set currently active row state to 'false'
         activeRow.dataset.active = 'false';
@@ -129,38 +208,72 @@ function evaluateWord() {
             const firstTile = nextRow.firstElementChild;
             firstTile.dataset.active = 'true';
         } else {
-            // Clear the game board and start back at the top
-            resetBoard();
+            // Player loses. On click, clear the game board and start back at the top
+            displayMessage("You Lose");
+            IS_PLAYING = false;
         }
+
     }
 
 }
 
-function displayError(message) {
+function animateReveal(tilesArray, index) {
+    if (index > 0) {
+        tilesArray[index - 1].firstElementChild.addEventListener('animationend', () => {
+            modifyTileAttributes(tilesArray, index);
+        });
+    } else {
+        modifyTileAttributes(tilesArray, index);
+    }
+}
 
-    // Query for modal 
-    const modal = document.querySelector(".modal-content");
+function modifyTileAttributes(tilesArray, index) {
+    tilesArray[index].dataset.animation = "flip";
+    tilesArray[index].firstElementChild.dataset.animation = "flip";
+    setTimeout(() => {
+        tilesArray[index].dataset.status = tilesArray[index].dataset.tempStatus;
+    }, 275);
+}
+
+function displayMessage(message) {
+
     // Query for modal text element and set text to content of message
-    modal.querySelector(".modal-text").textContent = message;
+    MODAL.querySelector(".modal-text").textContent = message;
     // Make modal visible
-    modal.style.display = 'block';
-
-    // Make modal invisible when mouse clicked
-    window.onclick = function() {
-        modal.style.display = 'none';
-    }
+    MODAL.style.display = 'block';
 
 }
 
-function resetBoard() {
+function startGame() {
 
-    // Clear CURRWORD
-    CURRWORD = "";
+    // Set solution word for this game
+    // TODO: add word list
+    CURR_WORD = "CYNIC";
 
-    // Query for all game tiles and clear out text content
+    // Set IS_PLAYING to true
+    IS_PLAYING = true;
+
+    // Set message modal display mode to 'none'
+    MODAL.style.display = 'none';
+
+    // Clear CURRGUESS
+    CURR_GUESS = "";
+
+    // Query for all game tiles and clear out text content, data-letter, data-status, and data-animation attributes
     const allTiles = document.querySelectorAll(".game-tile");
     allTiles.forEach(tile => {
-        tile.textContent = "";
+        tile.firstElementChild.textContent = "";
+        tile.dataset.letter = "";
+        tile.dataset.tempStatus = "";
+        tile.dataset.status = "";
+        tile.dataset.animation = "";
+        tile.firstElementChild.dataset.animation = "";
+    })
+
+    // Query for all virtual keys and clear out data-status attribute
+    const allKeys = document.querySelectorAll(".key");
+    allKeys.forEach(key => {
+        key.dataset.status = "";
     })
 
     // Query for first row and first tile in that row, and set active states to 'true'
